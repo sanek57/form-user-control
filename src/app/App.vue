eae6cbb3d09c75e7d94581e1b376554d000c5634
sanek57 <sanekofblack@gmail.com>
2026-02-03 13:00:51 +1000

    feat: add header component

---
+++ src/app/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div
+        class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10"
+    >
+        <div class="w-full max-w-2xl border-2 border-red-400">
+            <Header @add="console.log('add user');"/>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+
+</script>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-col justify-center gap-2">
+        <div class="flex items-center justify-around p-2">
+            <h1 class="text-4xl font-bold">Учетные записи</h1>
+            <Button
+                id="addUser"
+                variant="outline"
+                @click="$emit('add')"
+            >
+                <Plus />
+            </Button>
+        </div>
+        <div
+            class="grid grid-cols-[max-content_1fr] items-center gap-2 p-2 bg-muted rounded-sm"
+        >
+            <CircleQuestionMark />
+            <Label for="addUser"
+                >Для указания нескольких меток одной пары логин/пароль
+                используйте разделитель ;</Label
+            >
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+import { Plus, CircleQuestionMark } from 'lucide-vue-next'
+
+defineEmits<{
+    (e: 'add'): void
+}>()
+</script>
